05472e980c9d64bf9e8be539a152c29be5127b7a
avelard <avelard20@gmail.com>
2025-03-31 22:11:22 -0300

    31/03/2025

--- PW/js/index.js
+++ PW/js/index.js
@@ -1,24 +1,50 @@
 alert("Seja bem-vindo à SETIF 2025!");
 document.addEventListener('DOMContentLoaded', (event)=>{
     buscarInscritos();
+    construirModal();
+    const temaLocal = localStorage.getItem('tema');
+    document.body.setAttribute('data-theme', temaLocal);
 }) 
+
+function construirModal() {
+    const botaoSaibaMais = document.getElementById('btSaibaMais');
+    const modal = document.getElementById('modal');
+    const botaoFechar = document.getElementById('fecharModal');
+
+    botaoFechar.addEventListener('click', () => {
+        modal.classList.add('hidden');
+
+    });
+    botaoSaibaMais.addEventListener('click', () => {
+        modal.classList.remove('hidden');
+    });
+    window.addEventListener("click", (e) => {
+        console.log(e.target);
+        if(e.target == modal) {
+            modal.classList.add('hidden');
+        }
+    });
+
+};
+
 function alterarTema() {
     //DOM -> Document Object Model
     const tema = document.body.getAttribute('data-theme');
     const novoTema = tema == 'dark' ? 'light' : 'dark' ;
+    localStorage.setItem('tema', novoTema);
     document.body.setAttribute('data-theme', novoTema);
     const btAlterarTema = document.getElementById('btAlterarTema');
     btAlterarTema.textContent = novoTema == 'dark' ? 'Modo Claro' : 'Modo Escuro';
     
 }
 function buscarInscritos() {
-    fetch("https://jsonplaceholder.typicode.com/users")
+    fetch("json/inscritos.json")
     .then(res =>  res.json())
     .then(res => {
         const divInscritos = document.getElementById('inscritos');
         res.forEach(user => {
             const novoParagrago = document.createElement("p");
-            novoParagrago.textContent =`Nome: ${user.name}`;
+            novoParagrago.textContent =`Nome: ${user.nome}`;
             divInscritos.appendChild(novoParagrago);
     });
 
